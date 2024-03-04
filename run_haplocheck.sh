ed9a97cb0c93796f835f7ccdcff0af0b6c633c2c
Angelica Galianese <55033537+agalianese@users.noreply.github.com>
2024-03-04 14:39:29 -0500

    Update run_haplocheck.sh

--- run_haplocheck.sh
+++ run_haplocheck.sh
@@ -1,3 +1,3 @@
 
 
-./cloudgene run haplocheck@1.3.2 --files 10002F2.bam —-format bam —-output results --threads 10 > 10002F2.log 2> > 10002F2.error 
+./cloudgene run haplocheck@1.3.2  --format bam  --files /pmglocal/ag4555/AM/data/input-bams/ —-output results --threads 10 > 10002F2.log 2> > 10002F2.error 
